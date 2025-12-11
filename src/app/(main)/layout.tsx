19eb397e466c63dd8b87939cd1312b41191a1e9a
leehyunjun97 <monsterstamp79@gmail.com>
2025-12-12 00:41:48 +0900

    ✨ 사이드바 컴포넌트 추가 및 라우팅

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,3 +1,12 @@
+import Sidebar from "@/components/common/SideBar";
+
 export default function MainLayout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
+  return (
+    <div className="flex">
+      <Sidebar />
+      <div className="border-border-main h-screen w-full rounded-md border-[3px] border-l">
+        {children}
+      </div>
+    </div>
+  );
 }
