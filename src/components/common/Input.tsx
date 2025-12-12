aa868b9d81df9535b6ae77a961e8580bc94b1033
mandububsa <asm7179@gmail.com>
2025-12-12 10:05:58 +0900

    ✨ 인풋 퍼블리싱

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -1,7 +1,11 @@
-export default function Input () {
+export default function Input() {
   return (
     <>
-      <h1>Input Component</h1>
+      <input
+        type="email"
+        placeholder="이메일"
+        className="bg-input-area border-border-sub2 text-border-sub shadow-inner-soft w-full appearance-none rounded-xl border-2 px-4 py-3.5 text-[20px] outline-none focus:border-3"
+      />
     </>
   );
 }
