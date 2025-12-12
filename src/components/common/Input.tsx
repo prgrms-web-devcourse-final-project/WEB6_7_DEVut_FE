536da96c82677c0b54ed6339b79f7dcf077e24a5
mandububsa <asm7179@gmail.com>
2025-12-12 10:34:09 +0900

    ✨ 높이 오토리사이즈 적용한  textarea 퍼블리싱

--- src/components/common/Input.tsx
+++ src/components/common/Input.tsx
@@ -4,7 +4,7 @@ export default function Input() {
       <input
         type="email"
         placeholder="이메일"
-        className="bg-input-area border-border-sub2 text-border-sub shadow-inner-soft w-full appearance-none rounded-xl border-2 px-4 py-3.5 text-[20px] outline-none focus:border-3"
+        className="bg-input-area border-border-sub2 text-border-sub shadow-inner-soft text-4 mx-auto w-full max-w-sm appearance-none rounded-xl border-2 px-4 py-3.5 outline-none focus:border-3"
       />
     </>
   );
