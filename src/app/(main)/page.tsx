dc5885e049e04ae5717de83c5719be7bfaa733f0
singilwon <joowoni98@naver.com>
2025-12-12 11:29:45 +0900

    ✨상품카드 기능 추가

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,3 +1,9 @@
+import ProductCard from "@/components/common/ProductCard";
+
 export default function Home() {
-  return <div className="">안녕하세요</div>;
+  return (
+    <div className="p-5">
+      <ProductCard type={"done"} money={1165000} title={"천사맛 쿠키"} isLive />
+    </div>
+  );
 }
---
+++ src/components/ProductStatus.tsx
@@ -0,0 +1,16 @@
+import StatusBar, { TimeBar } from "./common/StatusBar";
+
+type Product = "pending" | "processing" | "done" | "confirmed" | "delay" | "offLive" | "onLive";
+
+type Status = "pending" | "processing" | "done" | "confirmed";
+type Time = "delay" | "offLive" | "onLive";
+
+export default function ProductStatus({ type, label }: { type: Product; label?: string }) {
+  const isStatus = ["pending", "processing", "done", "confirmed"].includes(type);
+  const isTime = ["delay", "offLive", "onLive"].includes(type);
+  if (isStatus) return <StatusBar status={type as Status} />;
+
+  if (isTime) return <TimeBar type={type as Time} label={label} />;
+
+  return null;
+}
