430991b99ab01d5ba3e5d08571f04cd0a33aa968
leehyunjun97 <monsterstamp79@gmail.com>
2025-12-12 21:27:25 +0900

    ✨마이페이지 내 정보 부분 UI 퍼블리싱

---
+++ src/components/mypage/MyBizzSimple.tsx
@@ -0,0 +1,7 @@
+import ContentContainer from "../common/ContentContainer";
+
+export default function MyBizzSimple() {
+  return (
+    <ContentContainer className="flex min-h-[370px] flex-col justify-between">hi</ContentContainer>
+  );
+}
