ab65efde18ad9d95d37e82943b1eb277718dcc66
leehyunjun97 <monsterstamp79@gmail.com>
2025-12-14 01:21:19 +0900

    ✨상품 카드 수정 및 일반 인기 상품 부분 UI 퍼블리싱

--- src/components/home/DelayPopularProducts.tsx
+++ src/components/home/DelayPopularProducts.tsx
@@ -1,9 +1,18 @@
 import Pagenation from "../common/Pagenation";
+import ProductCard from "../common/ProductCard";
+import Title from "../common/Title";
 
 export default function DelayPopularProducts() {
   return (
-    <>
+    <div className="mt-10">
+      <Title>일반 인기 상품</Title>
+      <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4">
+        <ProductCard money={100000} title="나이키 슈즈" type="onLive" />
+        <ProductCard money={100000} title="나이키 슈즈" type="onLive" />
+        <ProductCard money={100000} title="나이키 슈즈" type="onLive" />
+        <ProductCard money={100000} title="나이키 슈즈" type="onLive" />
+      </div>
       <Pagenation />
-    </>
+    </div>
   );
 }
