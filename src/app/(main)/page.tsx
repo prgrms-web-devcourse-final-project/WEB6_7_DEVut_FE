070445e9326c0c938c0b9be1834e471f933a726a
leehyunjun97 <monsterstamp79@gmail.com>
2025-12-14 00:37:25 +0900

    ✨title 컴포넌트 정의

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,13 +1,12 @@
 import ContentContainer from "@/components/common/ContentContainer";
 import Pagenation from "@/components/common/Pagenation";
+import Title from "@/components/common/Title";
 
 export default function Home() {
   return (
-    <div className="">
-      <ContentContainer>
-        홈 화면입니다.
-        <Pagenation />
-      </ContentContainer>
-    </div>
+    <ContentContainer className="border-none shadow-none">
+      <Title size={"lg"}>라이브 인기 상품</Title>
+      <Pagenation />
+    </ContentContainer>
   );
 }
---
+++ src/components/common/Title.tsx
@@ -0,0 +1,29 @@
+import { cva, VariantProps } from "class-variance-authority";
+import { ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
+
+const titleVariants = cva("text-title-main font-bold", {
+  variants: {
+    size: {
+      sm: "text-sm font-semibold",
+      md: "text-lg",
+      lg: "text-xl",
+    },
+  },
+  defaultVariants: {
+    size: "md",
+  },
+});
+
+interface TitleProps extends VariantProps<typeof titleVariants> {
+  children: ReactNode;
+  className?: string;
+  wrapperClassName?: string;
+}
+export default function Title({ children, size, className, wrapperClassName }: TitleProps) {
+  return (
+    <div className={twMerge("mb-4 flex items-center justify-between", wrapperClassName)}>
+      <h2 className={twMerge(titleVariants({ size }), className)}>{children}</h2>
+    </div>
+  );
+}
