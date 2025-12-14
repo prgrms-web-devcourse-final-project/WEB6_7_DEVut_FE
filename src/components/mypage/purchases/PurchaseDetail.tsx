879224b8ec1841a591d6002f08b2e0a2e9fdfa8c
leehyunjun97 <monsterstamp79@gmail.com>
2025-12-14 15:50:40 +0900

    ✨구매 상세 부분 스위치 추가

--- src/components/mypage/purchases/PurchaseDetail.tsx
+++ src/components/mypage/purchases/PurchaseDetail.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import OptionDropdown from "@/components/common/OptionDropdown";
+import OrderSwitch from "@/components/common/OrderSwitch";
 import Pagenation from "@/components/common/Pagenation";
 import ProductCard from "@/components/common/ProductCard";
 import Title from "@/components/common/Title";
@@ -11,7 +12,11 @@ export default function PurchaseDetail() {
   return (
     <div className="mt-10">
       <Title wrapperClassName="mb-0">구매 상세</Title>
-      <div className="w-full text-right">
+      <div className="flex w-full justify-end gap-5">
+        <div className="relative w-[110px] translate-y-2 scale-120">
+          <OrderSwitch />
+        </div>
+
         <OptionDropdown label={status}>
           <OptionDropdown.Item onClick={() => setStatus("전체")}>전체</OptionDropdown.Item>
           <OptionDropdown.Item onClick={() => setStatus("잔금대기")}>잔금대기</OptionDropdown.Item>
@@ -24,6 +29,7 @@ export default function PurchaseDetail() {
           </OptionDropdown.Item>
         </OptionDropdown>
       </div>
+
       <div className="mt-3 grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4">
         <ProductCard money={100000} title="나이키 슈즈" type="onLive" />
         <ProductCard money={100000} title="나이키 슈즈" type="onLive" />
