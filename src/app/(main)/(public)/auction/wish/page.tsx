6c056b1c006e0e7453bbaf1adbb989b793a032f1
leehyunjun97 <monsterstamp79@gmail.com>
2025-12-14 18:13:41 +0900

    ✨찜 목록 UI 퍼블리싱

--- src/app/(main)/(public)/auction/wish/page.tsx
+++ src/app/(main)/(public)/auction/wish/page.tsx
@@ -1,7 +1,17 @@
+import ContentContainer from "@/components/common/ContentContainer";
+import Pagenation from "@/components/common/Pagenation";
+import ProductCard from "@/components/common/ProductCard";
+
 export default function AuctionWishPage() {
   return (
-    <>
-      <h1>AuctionWishPage</h1>
-    </>
+    <ContentContainer bordered={false} className="pt-5">
+      <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4">
+        <ProductCard money={100000} title="나이키 슈즈" type="onLive" />
+        <ProductCard money={100000} title="나이키 슈즈" type="onLive" />
+        <ProductCard money={100000} title="나이키 슈즈" type="onLive" />
+        <ProductCard money={100000} title="나이키 슈즈" type="onLive" />
+      </div>
+      <Pagenation />
+    </ContentContainer>
   );
 }
