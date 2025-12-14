553001f493c3cc9bc2da65d9c6a79132d7a981dc
leehyunjun97 <monsterstamp79@gmail.com>
2025-12-14 14:20:02 +0900

    ✨마이 - 구매 페이지 입찰중인 목록 부분 UI 퍼블리싱

--- src/app/(main)/(private)/mypage/purchases/page.tsx
+++ src/app/(main)/(private)/mypage/purchases/page.tsx
@@ -1,7 +1,18 @@
+import ContentContainer from "@/components/common/ContentContainer";
+import BidList from "@/components/mypage/purchases/BidList";
+
 export default function PurchasesPage() {
   return (
-    <>
-      <h1>PurchasesPage</h1>
-    </>
+    <ContentContainer bordered={false} className="pt-5">
+      <div>
+        {/* <div className="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4">
+            <ProductCard money={100000} title="나이키 슈즈" type="onLive" />
+            <ProductCard money={100000} title="나이키 슈즈" type="onLive" />
+            <ProductCard money={100000} title="나이키 슈즈" type="onLive" />
+            <ProductCard money={100000} title="나이키 슈즈" type="onLive" />
+          </div> */}
+        <BidList />
+      </div>
+    </ContentContainer>
   );
 }
