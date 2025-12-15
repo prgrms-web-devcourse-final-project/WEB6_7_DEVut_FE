67158845a0b4ec4989940a92ba3648fc27f3fef1
leehyunjun97 <monsterstamp79@gmail.com>
2025-12-15 22:41:03 +0900

    ✨font 추가 및 라이브 경매방 목록 상단 UI 퍼블리싱

--- src/app/(main)/(public)/auction/page.tsx
+++ src/app/(main)/(public)/auction/page.tsx
@@ -1,12 +1,21 @@
+import StageBarBackground from "@/components/auction/live/liveRoom/stage/StageBarBackground";
+import StageTab from "@/components/auction/live/liveRoom/stage/StageTab";
 import PageTabArea from "@/components/common/PageTabArea";
 import { auctionItems } from "@/constants/route/auction";
 
 export default function AuctionPage() {
   return (
     <>
-      <PageTabArea items={auctionItems} />
-      <div className="mx-auto w-full max-w-[1440px]">
-        <h1>AuctionPage</h1>
+      <PageTabArea items={auctionItems} isUnderline={false} />
+      <div className="mx-auto flex min-h-screen w-[98%]">
+        <div className="w-full">
+          <StageBarBackground className="h-30 text-white">
+            <div className="font-aclonica flex w-full flex-col text-center text-4xl font-bold">
+              <span>2025-12-10 11:30</span>
+              <span>AUCTION</span>
+            </div>
+          </StageBarBackground>
+        </div>
       </div>
     </>
   );
