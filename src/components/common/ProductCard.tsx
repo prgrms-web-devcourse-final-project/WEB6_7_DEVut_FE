9bb0283628e4bc1e9e11aace803340b6b2c96d6f
leehyunjun97 <monsterstamp79@gmail.com>
2026-01-10 21:59:25 +0900

    ♻️ 찜 눌렀을 때 프로그래스바 반응 안되게 수정

--- src/components/common/ProductCard.tsx
+++ src/components/common/ProductCard.tsx
@@ -27,17 +27,15 @@ export default function ProductCard({
   const { mutate: toggleWish } = useWishToggle();
 
   return (
-    <Link href={product.href} className={twMerge("relative cursor-pointer", className)}>
+    <div className="relative">
       <button
         className="absolute top-5 left-5 z-20 transition-transform hover:scale-105 active:scale-90"
-        onClick={e => {
-          e.preventDefault();
-          e.stopPropagation();
+        onClick={() =>
           toggleWish({
             id: product.id,
             type: product.type === "LIVE" ? "LIVE" : "DELAYED",
-          });
-        }}
+          })
+        }
       >
         <Image
           src={product.isWish ? fullStar : emptyStar}
@@ -47,49 +45,55 @@ export default function ProductCard({
         />
       </button>
 
-      <div className="flex h-full w-full flex-col rounded-md border-2 border-[#4F382A] bg-[#FDF6E9] shadow-[1.5px_1.5px_0px_rgba(0,0,0,0.5)] transition-transform hover:scale-[1.01] active:scale-[0.99]">
-        <div className="relative aspect-214/134 w-full overflow-hidden rounded-[3px] p-2">
-          <WrapperImage src={product.image ?? test} alt={product.title} />
+      <Link href={product.href} className={twMerge("relative cursor-pointer", className)}>
+        <div className="flex h-full w-full flex-col rounded-md border-2 border-[#4F382A] bg-[#FDF6E9] shadow-[1.5px_1.5px_0px_rgba(0,0,0,0.5)] transition-transform hover:scale-[1.01] active:scale-[0.99]">
+          <div className="relative aspect-214/134 w-full overflow-hidden rounded-[3px] p-2">
+            <WrapperImage src={product.image ?? test} alt={product.title} />
 
-          {product.badge && (
-            <div className="absolute top-3 right-3 z-10">
-              <Image src={product.badge.image} alt={product.badge.alt} />
+            {product.badge && (
+              <div className="absolute top-3 right-3 z-10">
+                <Image src={product.badge.image} alt={product.badge.alt} />
+              </div>
+            )}
+            <div className="absolute right-3 bottom-3">
+              {((context === "MY_BUYING" && product.status?.kind === "status") ||
+                (context === "MY_SELLING" && product.status?.kind === "status")) &&
+              product.status?.kind === "status" &&
+              product.status?.status !== "BEFORE_BIDDING" &&
+              product.status?.status !== "FAILED" ? (
+                <Button
+                  className="max-h-8 px-3 py-1 text-[12px]"
+                  onClick={e => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    router.replace(`/trade/${product.type.toLowerCase()}/${product.dealId}`);
+                  }}
+                >
+                  거래 상세
+                </Button>
+              ) : null}
             </div>
-          )}
-          <div className="absolute right-3 bottom-3">
-            {((context === "MY_BUYING" && product.status?.kind === "status") ||
-              (context === "MY_SELLING" && product.status?.kind === "status")) &&
-            product.status?.kind === "status" &&
-            product.status?.status !== "BEFORE_BIDDING" &&
-            product.status?.status !== "FAILED" ? (
-              <Button
-                className="max-h-8 px-3 py-1 text-[12px]"
-                onClick={e => {
-                  e.preventDefault();
-                  e.stopPropagation();
-                  router.replace(`/trade/${product.type.toLowerCase()}/${product.dealId}`);
-                }}
-              >
-                거래 상세
-              </Button>
-            ) : null}
-          </div>
-        </div>
-
-        <div className="flex flex-1 flex-col px-2">
-          <div className="text-title-main-dark mt-2">
-            <p className="text-[12px] opacity-70">입찰가</p>
-            <BizzAmount amount={product.amount} className="text-custom-orange-dark font-bold" />
-            <p className="mt-2 line-clamp-2 min-h-10 text-[14px] leading-snug">{product.title}</p>
           </div>
 
-          {product.status && (
-            <div className="mt-auto mb-3 w-[98%] self-center">
-              <ProductStatus context={context} status={product.status} auctionType={product.type} />
+          <div className="flex flex-1 flex-col px-2">
+            <div className="text-title-main-dark mt-2">
+              <p className="text-[12px] opacity-70">입찰가</p>
+              <BizzAmount amount={product.amount} className="text-custom-orange-dark font-bold" />
+              <p className="mt-2 line-clamp-2 min-h-10 text-[14px] leading-snug">{product.title}</p>
             </div>
-          )}
+
+            {product.status && (
+              <div className="mt-auto mb-3 w-[98%] self-center">
+                <ProductStatus
+                  context={context}
+                  status={product.status}
+                  auctionType={product.type}
+                />
+              </div>
+            )}
+          </div>
         </div>
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 }
